2f1d41f32d986328459368f7955b1560aa5e3fda
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-10-28 01:32:40 +0530

    add code for NFT minting

--- scripts/run.js
+++ scripts/run.js
@@ -21,6 +21,15 @@ const main = async () => {
 
     // address of the deployed contract
     console.log('Contract deployed to:', gameContract.address);
+    let txn;
+    // We only have three characters.
+    // an NFT w/ the character at index 2 of our array.
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
+
+    // Get the value of the NFT's URI.
+    let returnedTokenUri = await gameContract.tokenURI(1);
+    console.log('Token URI:', returnedTokenUri);
 };
 
 const runMain = async () => {
